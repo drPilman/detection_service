124d96441cef9217230f98d51d4c8840ca47d409
dr.Pilman <doktorpilman@gmail.com>
2022-04-13 02:45:00 +0300

    add fast api

--- controller/src/main.py
+++ controller/src/main.py
@@ -1,12 +1,23 @@
 import docker
+from fastapi import FastAPI, HTTPException
+from .models import *
+import logging
 
+app = FastAPI()
 client = docker.from_env()
+logger = logging.getLogger(__name__)
 
 
-def add_tracker(url: str):
+@app.get('/')
+def health_check():
+    return {"msg": "OK"}
+
+
+@app.post('/add_tracker')
+def add_tracker(source: RSTP_URL):
     # docker run --rm --net="host" --gpus=all drpilman/detector rtsp://127.0.0.1:8554/mystream
     container = client.containers.run("drpilman/detector",
-                                      url,
+                                      source.url,
                                       network_mode="host",
                                       detach=True,
                                       auto_remove=True,
@@ -14,27 +25,48 @@ def add_tracker(url: str):
     # logs = container.logs(stream=True)
     # for s in logs:
     #    print(s)
-    return container.id, container.status
+    return [container.id, container.status]
 
 
+@app.post('/list_trackers')
 def list_trackers():
-    return client.containers.list(all=True, filters={"ancestor": "drpilman/detector"})
+    containers = client.containers.list(all=True, filters={"ancestor": "drpilman/detector"})
+    return {
+        'trackers': [{'full_id': container.id, 'status': container.status} for container in containers],
+        'count': len(containers)
+    }
 
 
-def tracker(str_id):
-    return client.containers.get(str_id)
+def get_tracker(str_id):
+    try:
+        return client.containers.get(str_id)
+    except docker.errors.NotFound:
+        raise HTTPException(status_code=404, detail="Item not found")
 
 
-def remove_tracker(str_id):
-    tracker(str_id).remove(force=True)
+@app.post('/tracker_info')
+def tracker_info(tracker: SHA_ID):
+    return {'msg': 'not implemented yet'}
 
 
-def pause_tracker(str_id):
-    tracker(str_id).pause()
+@app.post('/remove_tracker')
+def remove_tracker(tracker: SHA_ID):
+    get_tracker(tracker.full_id).remove(force=True)
 
 
-def unpause_tracker(str_id):
-    tracker(str_id).unpause()
+@app.post('/pause_tracker')
+def pause_tracker(tracker: SHA_ID):
+    get_tracker(tracker.full_id).pause()
+
+
+@app.post('/unpause_tracker')
+def unpause_tracker(tracker: SHA_ID):
+    get_tracker(tracker.full_id).unpause()
+
 
 # ./rtsp-simple-server
 # ffmpeg -re -stream_loop -1 -i test.m4v -c copy -f rtsp rtsp://localhost:8554/mystream
+
+@app.post('/log_result')
+def tracker_result(frame: ResultData):
+    logger.info(frame)
