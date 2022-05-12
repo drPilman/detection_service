8cf1839a3e41155211d7a129be307cdc7379dacb
neafanasev <neafanasev@gmail.com>
2022-05-12 11:51:27 +0300

    websocket bug fixed

--- web/src/components/Video/Video.jsx
+++ web/src/components/Video/Video.jsx
@@ -1,21 +1,23 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useParams} from "react-router-dom";
 
 const Video = (props) => {
     const params = useParams()
-    let [imgSrc, setImgSrc] = useState('')
-    let ws = new WebSocket(`ws://drpilman.ga:8000/ws/${params.streamId}`)
 
-    const onImgLoad = (e) => {
-        URL.revokeObjectURL(this.src)
-    }
-    ws.onmessage = (e) => {
-        setImgSrc(URL.createObjectURL(e.data))
-    }
+    useEffect(async () => {
+        let ws = await new WebSocket(`ws://drpilman.ga:8000/ws/${params.streamId}`)
+
+        ws.onmessage = (e) => {
+            setImgSrc(URL.createObjectURL(e.data))
+        }
+
+    }, [])
+
+    let [imgSrc, setImgSrc] = useState('')
 
     return (
         <div>
-            <img src={imgSrc} id={'frame'} onLoad={onImgLoad}/>
+            <img src={imgSrc} id={'frame'} onLoad={(e) =>  {URL.revokeObjectURL(this.src)}}/>
         </div>
     )
 }
