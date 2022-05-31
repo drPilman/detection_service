f57c9cce2dc2f6f68dbfa60f3ed793892951f487
dr.Pilman <doktorpilman@gmail.com>
2022-05-31 18:56:34 +0300

    prod npm + new video + 90 port + fix download

--- README.md
+++ README.md
@@ -1,31 +1,49 @@
-# Requirements
+# Detection web service
+
+## Requirements
+
 - [docker](https://www.docker.com/)
 - [docker compose](https://github.com/docker/compose)
 - [nvidia-docker](https://github.com/NVIDIA/nvidia-docker) (if not for test)
 
-# Build
+## Build
+
 ```bash
 docker compose -f build.yml build
 ```
 
-# RUN
+## RUN
+
 ```bash
 docker compose up
 ```
 
-# Test
+## Test
+
 ```bash
 docker compose -f build.test.yml build
 ```
+
 run test service (light detector)
+
 ```bash
 docker compose -f test.yml up --build
 ```
 
-run single pytest 
+run single pytest
 
 ```bash
 docker compose -f test_api.yml run pytest
 ```
-# NOTE
-if you use `sudo`, you should change `STORE_PATH=${PWD}/store` to abs path dir `store` of this project, because ${PWD} in sudo mode don't return this path.
+
+## NOTE
+
+- If you use `sudo`, you should change `STORE_PATH=${PWD}/store` to abs path dir `store` of this project, because ${PWD} in sudo mode don't return this path.
+
+- If you want access to web over you local network, you shoud set up var API_HOST to your local adress (e.g '192.168.1.5'). you can do it with:
+
+```bash
+export API_HOST=$(hostname -I | cut -d' ' -f1)
+```
+
+- Default test rtsp avalible on rtsp://localhost:8554/stream
--- web/src/components/ListDownloads/ListDownloads.jsx
+++ web/src/components/ListDownloads/ListDownloads.jsx
@@ -1,30 +1,30 @@
-import React, {useEffect} from 'react'
-import {connect} from "react-redux";
-import {getListDownloadsTC} from "./../../redux/app-reducer";
+import React, { useEffect } from 'react'
+import { connect } from "react-redux";
+import { getListDownloadsTC } from "./../../redux/app-reducer";
 import s from './ListDownloads.module.scss'
 import s1 from './../../common/button.module.scss'
-
+import base_url from "../../api/base_url"
 
 const ListDownloads = (props) => {
     //make upload list
     const getListDownloads = props.getListDownloadsTC
 
-    useEffect(async ()=> {
+    useEffect(async () => {
         await getListDownloads()
     }, [])
 
     let downloadsC = props.listDownloads.map(
         item => (<tr key={item}>
             <td>{item}</td>
-            <td><button onClick={() => {window.open('http://127.0.0.1:8000/download/'+item, '_blank')}}
-                        className={s1.btn}>Download</button></td>
+            <td><button onClick={() => { window.open('http://' + base_url + '/download/' + item, '_blank') }}
+                className={s1.btn}>Download</button></td>
         </tr>)
     )
 
     downloadsC.push(<tr key='test'>
         <td></td>
         <td><button onClick={getListDownloads}
-                    className={s1.btn}>Update</button>
+            className={s1.btn}>Update</button>
         </td>
     </tr>)
 
